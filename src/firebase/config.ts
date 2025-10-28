a067e39d5d2d1468095d79c63b944a0b7232e89b
max wilms <max.wilms24@gmail.com>
2025-10-28 10:06:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1538708355-ca29c",
+  "appId": "1:714082339065:web:11f0f50c99b2c88093fa28",
+  "apiKey": "AIzaSyBBWnHbwenj-DfHpKL8MxYQJqHXmPLCRSA",
+  "authDomain": "studio-1538708355-ca29c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "714082339065"
+};
